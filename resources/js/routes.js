590e7112979a83bdae81c92acb5f471a0b6e377c
olech3000@gmail.com <301276o>
2018-12-24 23:10:23 +0200

    Add Vue components, customize Admin LTE

---
+++ resources/js/routes.js
@@ -0,0 +1,4 @@
+export let routes = [
+    { path:'/dashboard', component: require('./components/Dashboard.vue').default },
+    { path:'/profile', component: require('./components/Profile.vue').default }
+];
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,11 +11,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title>LTVLmedia Admin Panel</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
@@ -68,15 +69,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <router-link to="/dashboard" class="nav-link">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Dashboard
                             </p>
-                        </a>
+                        </router-link>
                     </li>
                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link active">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-cog"></i>
                             <p>
                                 Management
@@ -85,7 +86,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
+                                <a href="#" class="nav-link">
                                     <i class="fas fa-circle-o nav-icon"></i>
                                     <p>Active Page</p>
                                 </a>
@@ -99,20 +100,30 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <router-link to="/profile" class="nav-link">
                             <i class="nav-icon fas fa-user"></i>
                             <p>
                                 Profile
                             </p>
-                        </a>
+                        </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        {{--<a href="#" class="nav-link">--}}
+                            {{--<i class="nav-icon fas fa-power-off"></i>--}}
+                            {{--<p>--}}
+                                {{--Logout--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
                             <i class="nav-icon fas fa-power-off"></i>
-                            <p>
-                                Logout
-                            </p>
+                            <p>{{ __('Logout') }}</p>
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
 
                 </ul>
@@ -126,8 +137,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="content-wrapper">
         <div class="content">
             <div class="container-fluid">
+                <router-view></router-view>
             </div><!-- /.container-fluid -->
-        </div>
+            </div>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
